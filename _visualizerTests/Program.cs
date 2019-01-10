d89b0dd9c19d6340f325a1301765aeee4649a2f2
Zev Spitz <shivisi@hotmail.com>
2019-01-10 03:49:48 +0200

    Test project for visualizer

---
+++ _visualizerTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using ExpressionTreeVisualizer;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.CodeAnalysis.LanguageNames;
+
+namespace _visualizerTests {
+    class Program {
+        [STAThread]
+        static void Main(string[] args) {
+            var i = 7;
+            var j = 8;
+            Expression<Func<bool>> expr = () => i * j <= 25;
+
+            var data = new VisualizerData(expr, CSharp);
+
+            var visualizerHost = new VisualizerDevelopmentHost(data, typeof(Visualizer));
+            visualizerHost.ShowVisualizer();
+        }
+    }
+}
