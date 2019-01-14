5ecae53e62dc84a6aa96b7f9994e4c54a8fd3400
Zev Spitz <shivisi@hotmail.com>
2019-01-14 06:13:57 +0200

    Test visualizer using VB-compiler-generated expressions

---
+++ _visualizerTests.VB/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Linq.Expressions
+Imports ExpressionTreeVisualizer
+Imports Microsoft.VisualStudio.DebuggerVisualizers
+Imports System.Linq.Expressions.Expression
+
+Module Module1
+
+    Sub Main()
+        Dim i = 7
+        Dim j = 5
+        Dim expr As Expression(Of Func(Of Integer)) = Function() i * j
+        Dim data = New VisualizerData(expr, "C#")
+        Dim visualizerHost = New VisualizerDevelopmentHost(data, GetType(Visualizer))
+        visualizerHost.ShowVisualizer()
+    End Sub
+
+End Module
