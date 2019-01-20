755072146a4db135d52606a70f12b6d8f515be97
Zev Spitz <shivisi@hotmail.com>
2019-01-20 08:45:10 +0200

    Visualizer for testing; VB testing project

---
+++ _testVisualizer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("_testVisualizer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_testVisualizer")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("373720e1-593e-4023-a60c-cd52d2dc33b9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- _visualizerTests/Program.cs
+++ _visualizerTests/Program.cs
@@ -2,32 +2,39 @@
 using Microsoft.VisualStudio.DebuggerVisualizers;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using static Microsoft.CodeAnalysis.LanguageNames;
-using static System.Linq.Expressions.Expression;
 
 namespace _visualizerTests {
     class Program {
         [STAThread]
         static void Main(string[] args) {
-            var i = 7;
-            var j = 8;
+            //var i = 7;
+            //var j = 8;
             //Expression<Func<int>> expr = () => i + j;
 
-            var dte = DateTime.Now;
+            //Expression<Func<bool>> expr = () => i * j <= 25 || new DateTime(1, 1, 1981).Year >= j && new { DateTime.Now }.Now.Day > 10;
 
-            //Expression<Func<bool>> expr = () => i * j <= 25 || new DateTime(1,1,1981).Year >= j && new { DateTime.Now }.Now.Day > 10;
-
-            Expression<Func<int, int>> expr = x => Enumerable.Range(1, x).Select(y => x * y).Count();
+            //Expression<Func<int, int>> expr = x => Enumerable.Range(1, x).Select(y => x * y).Count();
 
             //var expr1 = Lambda(Constant(new DateTime(1980, 1, 1)));
 
-            var data = new VisualizerData(expr, VisualBasic);
+            Expression<Func<Foo>> expr = () => new Foo("baz") { Bar = "bar" };
+
+            //Expression<Func<List<string>>> expr = () => new List<string> { "abcd", "defg" };
+
+            var data = new VisualizerData(expr, CSharp);
             var visualizerHost = new VisualizerDevelopmentHost(data, typeof(Visualizer));
             visualizerHost.ShowVisualizer();
+
+            Console.ReadKey(true);
+        }
+    }
+
+    class Foo {
+        public string Bar { get; set; }
+        public Foo(string baz) {
+
         }
     }
 }
