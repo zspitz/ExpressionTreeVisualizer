f8f6c6cf629ff1d8fbb3903500050ba3eb649a09
Zev Spitz <shivisi@hotmail.com>
2019-07-31 00:07:14 +0300

    Define Type trait on class for expression tests

--- Tests.Common.VB/CompilerGenerated/VBCompilerGenerated.vb
+++ Tests.Common.VB/CompilerGenerated/VBCompilerGenerated.vb
@@ -1,4 +1,5 @@
 ﻿<Trait("Source", "VBCompilerGenerated")>
+<Trait("Type", "Expression object")> ' TODO ideally this would be on the base class, but https://github.com/xunit/xunit/issues/1397
 Public MustInherit Class VBCompilerGeneratedBase
     Inherits TestsBase
 End Class
--- Tests.Common/CompilerGenerated/CompilerGenerated.cs
+++ Tests.Common/CompilerGenerated/CompilerGenerated.cs
@@ -3,5 +3,6 @@ using static ExpressionToString.Tests.Globals;
 
 namespace ExpressionToString.Tests {
     [Trait("Source", CSharpCompiler)]
+    [Trait("Type", "Expression object")] // TODO ideally this would be on the base class, but https://github.com/xunit/xunit/issues/1397
     public abstract partial class CompilerGeneratedBase : TestsBase { }
 }
