c80f09385057d4095ab103b1fd3805ba64e985f4
Zev Spitz <shivisi@hotmail.com>
2019-08-11 18:21:03 +0300

    Progress on #71 - Constructed labels

--- Tests.Common/Constructed/MakeLabel.cs
+++ Tests.Common/Constructed/MakeLabel.cs
@@ -7,100 +7,16 @@ namespace ExpressionToString.Tests {
     public partial class ConstructedBase {
         [Fact]
         [Trait("Category", Labels)]
-        public void ConstructLabel() => RunTest(
-            // we're using variables here to force explicit blocks, which have indentation
-            // in order to verify that the label is written without indentation
-            Block(
-                new[] { i },
-                Block(
-                    new[] { j },
-                    Constant(true),
-                    Label(Label("target")),
-                    Constant(true)
-                )
-            ),
-            @"(
-    int i,
-    (
-        int j,
-        true,
-target:
-        true
-    )
-)",
-            @"Block
-    Dim i As Integer
-    Block
-        Dim j As Integer
-        True
-target:
-        True
-    End Block
-End Block",
-            @"Block(new[] { i },
-    Block(new[] { j },
-        Constant(true),
-        Label(
-            Label(""target""),
-            null
-        ),
-        Constant(true)
-    )
-)"
-        );
+        public void ConstructLabel() => PreRunTest();
 
         [Fact]
         [Trait("Category", Labels)]
-        public void ConstructLabel1() => RunTest(
-            Block(
-                new[] { i },
-                Block(
-                    new[] { j },
-                    Label(Label("target")),
-                    Constant(true)
-                )
-            ),
-            @"(
-    int i,
-    (
-        int j,
-target:
-        true
-    )
-)",
-            @"Block
-    Dim i As Integer
-    Block
-        Dim j As Integer
-target:
-        True
-    End Block
-End Block",
-            @"Block(new[] { i },
-    Block(new[] { j },
-        Label(
-            Label(""target""),
-            null
-        ),
-        Constant(true)
-    )
-)"
-        );
+        public void ConstructLabel1() => PreRunTest();
 
         [Fact]
-        public void ConstructLabelTarget() => RunTest(
-            Label("target"),
-            "target",
-            "target", 
-            @"Label(""target"")"
-        );
+        public void ConstructLabelTarget() => PreRunTest();
 
         [Fact]
-        public void ConstructEmptyLabelTarget() => RunTest(
-            Label(""),
-            "",
-            "",
-            @"Label("""")"
-        );
+        public void ConstructEmptyLabelTarget() => PreRunTest();
     }
 }
