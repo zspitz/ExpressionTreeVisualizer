9ba6624e897c8f2ea8417e63db1b8633d7933eb6
Zev Spitz <shivisi@hotmail.com>
2020-06-15 17:21:16 +0300

    Use VisualizerObjectSourceBase

--- Debuggee/VisualizerDataObjectSource.cs
+++ Debuggee/VisualizerDataObjectSource.cs
@@ -1,18 +1,11 @@
 ﻿using Microsoft.VisualStudio.DebuggerVisualizers;
 using System.IO;
 using ExpressionTreeVisualizer.Serialization;
+using Periscope.Debuggee;
 
 namespace ExpressionTreeVisualizer {
-    public class VisualizerDataObjectSource : VisualizerObjectSource {
-        static VisualizerDataObjectSource() => Periscope.Debuggee.SubfolderAssemblyResolver.Hook("ExpressionTreeVisualizer");
-
-        public override void GetData(object target, Stream outgoingData) =>
-            Serialize(outgoingData, "");
-
-        public override void TransferData(object target, Stream incomingData, Stream outgoingData) {
-            var config = (Config)Deserialize(incomingData);
-            var serializationModel = new VisualizerData(target, config);
-            Serialize(outgoingData, serializationModel);
-        }
+    public class VisualizerDataObjectSource : VisualizerObjectSourceBase<object, Config> {
+        static VisualizerDataObjectSource() => SubfolderAssemblyResolver.Hook("ExpressionTreeVisualizer");
+        public override object GenerateResponse(object target, Config config) => new VisualizerData(target, config);
     }
 }
