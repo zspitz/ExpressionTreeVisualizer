533e23474f464303070790c396f957e1a9ae8c31
Zev Spitz <shivisi@hotmail.com>
2020-09-09 21:53:47 +0300

    Update Periscope.Debuggee

--- Debuggee/VisualizerDataObjectSource.cs
+++ Debuggee/VisualizerDataObjectSource.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.VisualStudio.DebuggerVisualizers;
-using System.IO;
-using ExpressionTreeVisualizer.Serialization;
+﻿using ExpressionTreeVisualizer.Serialization;
 using Periscope.Debuggee;
 
 namespace ExpressionTreeVisualizer {
     public class VisualizerDataObjectSource : VisualizerObjectSourceBase<object, Config> {
         static VisualizerDataObjectSource() => SubfolderAssemblyResolver.Hook("ExpressionTreeVisualizer");
-        public override object GenerateResponse(object target, Config config) => new VisualizerData(target, config);
+        public override object GetResponse(object target, Config config) => new VisualizerData(target, config);
     }
 }
